67089737f295afbacd6994fbc2db89b503a6fabe
Adelina20034 <129692221+Adelina20034@users.noreply.github.com>
2023-04-07 21:50:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,11 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     n = len(x)
-    sd1 = np.std(x)
-    t = st.t.ppf(p,n-1)
-    return sd1 * math.sqrt(2*n) / (math.sqrt(2*n-3) + t), sd1 * math.sqrt(2*n) / (math.sqrt(2*n-3) - t)
+    t = st.t.ppf(p, 1)
+    a1 = np.sqrt(2 * n) / (np.sqrt(2 * n - 3) + t) * 6
+    a1 = np.sqrt(a1) if a1 > 0 else 0
+    a2 = np.sqrt(2 * n) / (np.sqrt(2 * n - 3) - t) * 6
+    a2 = np.sqrt(a2) if a2 > 0 else np.sqrt(6)
+    return a1, a2
+
     
